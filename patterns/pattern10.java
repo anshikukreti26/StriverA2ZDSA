9d6a47f96df06b86085d2b5e85bed3982d36642c
anshikukreti26 <96012835+anshikukreti26@users.noreply.github.com>
2022-12-31 09:25:19 +0530

    Create pattern10.java

---
+++ patterns/pattern10.java
@@ -0,0 +1,34 @@
+/*
+* 
+* * 
+* * * 
+* * * * 
+* * * * *
+* * * *
+* * *
+* *
+*
+*/
+
+class Solution {
+
+    void printTriangle(int n) {
+        // code here
+        for(int i=1; i<=n; i++)
+        {
+            for(int j=1; j<=i; j++)
+            {
+                System.out.print("* ");
+            }
+            System.out.println();
+        }
+        for(int i=n-1; i>=1; i--)
+        {
+            for(int j=1; j<=i; j++)
+            {
+                System.out.print("* ");
+            }
+            System.out.println();
+        }
+    }
+}
