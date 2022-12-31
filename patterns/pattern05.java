9c70656849e7b9ad63f932edfe89ab3d1c9de57c
anshikukreti26 <96012835+anshikukreti26@users.noreply.github.com>
2022-12-31 09:18:57 +0530

    Create pattern05.java

---
+++ patterns/pattern05.java
@@ -0,0 +1,22 @@
+/*
+* * * * *
+* * * * 
+* * * 
+* *  
+* 
+*/
+
+class Solution {
+
+    void printTriangle(int n) {
+        // code here
+        for(int i=n; i>0; i--)
+        {
+            for(int j=0; j<i; j++)
+            {
+                System.out.print("* ");
+            }
+            System.out.println();
+        }
+    }
+}
