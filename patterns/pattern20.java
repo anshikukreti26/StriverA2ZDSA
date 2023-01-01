e6a23f2c8bac399a0aac01c16be77a977bd32f5f
anshikukreti26 <96012835+anshikukreti26@users.noreply.github.com>
2023-01-01 17:10:11 +0530

    Create pattern20.java

---
+++ patterns/pattern20.java
@@ -0,0 +1,39 @@
+/*
+*        *
+**      **
+***    ***
+****  ****
+**********
+****  ****
+***    ***
+**      **
+*        *
+*/
+
+class Solution {
+
+    void printTriangle(int n) {
+        // code here
+        int i,j;
+        for(i=1; i<=n; i++)
+        {
+            for(j=1; j<=i; j++)
+               System.out.print("*");
+            for(j=1; j<=2*(n-i); j++)
+               System.out.print(" ");
+            for(j=1; j<=i; j++)
+               System.out.print("*");
+            System.out.println();
+        }
+        for(i=n-1; i>=1; i--)
+        {
+            for(j=1; j<=i; j++)
+               System.out.print("*");
+            for(j=1; j<=2*(n-i); j++)
+               System.out.print(" ");
+            for(j=1; j<=i; j++)
+               System.out.print("*");
+            System.out.println();
+        }
+    }
+}
