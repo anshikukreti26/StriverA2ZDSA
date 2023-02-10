ae9ddc0872d9c2e591b7b6f3114d454b07fe42cc
anshikukreti26 <96012835+anshikukreti26@users.noreply.github.com>
2023-02-10 14:06:22 +0530

    Create Print 1 to N using recursion.java

---
+++ basic recursion/Print 1 to N using recursion.java
@@ -0,0 +1,13 @@
+class Solution
+{
+    int k=1;
+  public void printNos(int N)
+    {
+        //Your code here
+        if(N==0)
+        return;
+        System.out.print(k + " ");
+        k++;
+        printNos(N-1);
+    }
+}
